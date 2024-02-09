4e8049087da488264a51763803cd74038f2f7de0
Tobias Goerke <tobias.goerke@gmail.com>
2024-02-09 11:24:30 +0100

    test: extending tests

--- integration-tests/environment/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-kustomize build $(dirname "$0") | kubectl apply -f -
-
-echo "Waiting for all deployments to be ready"
-kubectl wait --for=condition=available --timeout=600s deployment --all --all-namespaces
