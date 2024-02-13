c2af1b4a6a8e857868bb6aa9c42e12ecb8975506
Tobias Goerke <tobias.goerke@gmail.com>
2024-02-13 10:31:39 +0100

    feat: add custom runner

---
+++ src/test/java/de/viadee/bpm/camunda/connectors/kubeflow/integration/SomeIntegrationTest.java
@@ -0,0 +1,11 @@
+package de.viadee.bpm.camunda.connectors.kubeflow.integration;
+
+import org.junit.jupiter.api.Test;
+
+
+public class SomeIntegrationTest {
+
+  @Test
+  public void testSomeService() {
+  }
+}
