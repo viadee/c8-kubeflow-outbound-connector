f6420571b055cb7c4a9c20490bd14d87dffdea98
Andre Strothmann <andre.strothmann@viadee.de>
2024-02-23 16:38:40 +0100

    feat: unit test authUtil

--- src/test/java/de/viadee/bpm/camunda/connectors/kubeflow/services/KubeflowConnectorExecutorTest.java
+++ src/test/java/de/viadee/bpm/camunda/connectors/kubeflow/services/KubeflowConnectorExecutorTest.java
@@ -1,8 +1,10 @@
 package de.viadee.bpm.camunda.connectors.kubeflow.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
+import de.viadee.bpm.camunda.connectors.kubeflow.auth.EnvironmentAuthentication;
 import de.viadee.bpm.camunda.connectors.kubeflow.auth.NoAuthentication;
 import de.viadee.bpm.camunda.connectors.kubeflow.entities.KubeflowConnectorRequest;
 import de.viadee.bpm.camunda.connectors.kubeflow.entities.input.Configuration;
@@ -11,6 +13,7 @@ import de.viadee.bpm.camunda.connectors.kubeflow.entities.input.Timeout;
 import de.viadee.bpm.camunda.connectors.kubeflow.enums.KubeflowApiOperationsEnum;
 import de.viadee.bpm.camunda.connectors.kubeflow.enums.KubeflowApisEnum;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -40,4 +43,25 @@ class KubeflowConnectorExecutorTest {
     assertEquals("http://localhost:8080", kubeflowConnectorExecutor.kubeflowUrl); // env. configured in pom.xml via surefire plugin
     assertEquals("testNamespace", kubeflowConnectorExecutor.kubeflowMultiNs); // env. configured in pom.xml via surefire plugin
   }
+
+  @Test
+  void testAuthViaEnvironment() {
+    // given
+    var kubeflowConnectorRequest = new KubeflowConnectorRequest(
+        new EnvironmentAuthentication(),
+        new Configuration("http://localhost:8080", "testNamespace"),
+        kubeflowApiMock,
+        new Timeout(20)
+    );
+    // when
+    KubeflowConnectorExecutor kubeflowConnectorExecutor = new KubeflowConnectorExecutor(kubeflowConnectorRequest, 123456789,
+        KubeflowApisEnum.PIPELINES_V1, KubeflowApiOperationsEnum.GET_PIPELINES);
+    // then
+    assertTrue(kubeflowConnectorRequest.getAuthentication() instanceof EnvironmentAuthentication);
+
+  }
+
+
+
+  // test basic auth, bearer auth, test oauth 2.0
 }
---
+++ src/test/java/de/viadee/bpm/camunda/connectors/kubeflow/utils/AuthUtilTest.java
@@ -0,0 +1,47 @@
+package de.viadee.bpm.camunda.connectors.kubeflow.utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import de.viadee.bpm.camunda.connectors.kubeflow.auth.BearerAuthentication;
+import de.viadee.bpm.camunda.connectors.kubeflow.auth.EnvironmentAuthentication;
+import de.viadee.bpm.camunda.connectors.kubeflow.auth.OAuthAuthenticationClientCredentialsFlow;
+import de.viadee.bpm.camunda.connectors.kubeflow.entities.KubeflowConnectorRequest;
+import de.viadee.bpm.camunda.connectors.kubeflow.entities.input.Configuration;
+import de.viadee.bpm.camunda.connectors.kubeflow.entities.input.KubeflowApi;
+import de.viadee.bpm.camunda.connectors.kubeflow.entities.input.Timeout;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AuthUtilTest {
+
+  KubeflowApi kubeflowApiMock;
+
+  @BeforeEach
+  public void setup() {
+    kubeflowApiMock = mock(KubeflowApi.class);
+  }
+
+  @Test
+  void setAuthenticationParameters() {
+    // given
+    var kubeflowConnectorRequest = new KubeflowConnectorRequest(
+        new EnvironmentAuthentication(),
+        new Configuration("http://localhost:8080", "testNamespace"),
+        kubeflowApiMock,
+        new Timeout(20)
+    );
+    // when
+    AuthUtil.setAuthenticationParameters(kubeflowConnectorRequest);
+    // then (read expected values from env. vars that are set in pom.xml)
+    assertTrue(kubeflowConnectorRequest.getAuthentication() instanceof OAuthAuthenticationClientCredentialsFlow);
+    assertEquals("https//idProviderEndpoint.de", ((OAuthAuthenticationClientCredentialsFlow) kubeflowConnectorRequest.getAuthentication()).getOauthTokenEndpoint());
+    assertEquals("1", ((OAuthAuthenticationClientCredentialsFlow) kubeflowConnectorRequest.getAuthentication()).getClientId());
+    assertEquals("##23#43#87##23#", ((OAuthAuthenticationClientCredentialsFlow) kubeflowConnectorRequest.getAuthentication()).getClientSecretCC());
+    assertEquals("testScope", ((OAuthAuthenticationClientCredentialsFlow) kubeflowConnectorRequest.getAuthentication()).getScopes());
+    assertEquals("testAudience", ((OAuthAuthenticationClientCredentialsFlow) kubeflowConnectorRequest.getAuthentication()).getAudience());
+    assertEquals("testOAuthClient", ((OAuthAuthenticationClientCredentialsFlow) kubeflowConnectorRequest.getAuthentication()).getClientAuthentication());
+  }
+
+}
